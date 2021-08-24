9157f7e6552ab9c4a305047da02d7c7f0a5da534
taichi watanabe <tomato3713@outlook.com>
2021-08-24 18:20:20 +0900

    Add comment

--- struct.go
+++ struct.go
@@ -13,26 +13,26 @@ type LicenseInfo struct {
 	No                      int
 	Name                    string
 	TdfkCd                  string
-	Note                    string
-	Address                 string
-	ValidTerms              time.Time
-	RadioEquipmentLocation  string
-	RadioSpec               RadioSpec
+	Note                    string    // Detail only
+	Address                 string    // Detail only
+	ValidTerms              time.Time // Detail only
+	RadioEquipmentLocation  string    // Detail only
+	RadioSpec               RadioSpec // Detail only
 	RadioStationPurpose     string
-	PermittedOperatingHours string
+	PermittedOperatingHours string // Detail only
 	LicenseDate             time.Time
-	BroadcastMatter         string
+	BroadcastMatter         string // Detail only
 	CommMatter              string
-	Office                  string
-	CommPartner             string
-	StartingLimit           string
-	BroadcastDistrict       string
-	WorkPersonName          string
-	IdentificationSignals   string
-	RadioStationNumber      string
-	RadioStationCategory    string
-	MovementArea            string
-	LicenseNumber           string
+	Office                  string // Detail only
+	CommPartner             string // Detail only
+	StartingLimit           string // Detail only
+	BroadcastDistrict       string // Detail only
+	WorkPersonName          string // Detail only
+	IdentificationSignals   string // Detail only
+	RadioStationNumber      string // Detail only
+	RadioStationCategory    string // Detail only
+	MovementArea            string // Detail only
+	LicenseNumber           string // Detail only
 }
 
 // RadioSpec is the structure consisting of radio wave type, frequency, and
