57958f053060c1bf31736957b943672445ac6331
taichi watanabe <tomato3713@outlook.com>
2021-08-24 18:32:35 +0900

    Add RegistrationList type for registration list get api

--- struct.go
+++ struct.go
@@ -2,6 +2,14 @@ package soumuradio
 
 import "time"
 
+// RadioSpec is the structure consisting of radio wave type, frequency, and
+// antenna power.
+type RadioSpec struct {
+	RadioFormat string
+	Freq        float64
+	Power       float64
+}
+
 type LicenseList struct {
 	LicenseInfo    []LicenseInfo
 	TotalCount     int
@@ -13,32 +21,44 @@ type LicenseInfo struct {
 	No                      int
 	Name                    string
 	TdfkCd                  string
+	RadioStationPurpose     string
+	LicenseDate             time.Time
+	CommMatter              string
 	Note                    string    // Detail only
 	Address                 string    // Detail only
 	ValidTerms              time.Time // Detail only
 	RadioEquipmentLocation  string    // Detail only
 	RadioSpec               RadioSpec // Detail only
-	RadioStationPurpose     string
-	PermittedOperatingHours string // Detail only
-	LicenseDate             time.Time
-	BroadcastMatter         string // Detail only
-	CommMatter              string
-	Office                  string // Detail only
-	CommPartner             string // Detail only
-	StartingLimit           string // Detail only
-	BroadcastDistrict       string // Detail only
-	WorkPersonName          string // Detail only
-	IdentificationSignals   string // Detail only
-	RadioStationNumber      string // Detail only
-	RadioStationCategory    string // Detail only
-	MovementArea            string // Detail only
-	LicenseNumber           string // Detail only
+	PermittedOperatingHours string    // Detail only
+	BroadcastMatter         string    // Detail only
+	Office                  string    // Detail only
+	CommPartner             string    // Detail only
+	StartingLimit           string    // Detail only
+	BroadcastDistrict       string    // Detail only
+	WorkPersonName          string    // Detail only
+	IdentificationSignals   string    // Detail only
+	RadioStationNumber      string    // Detail only
+	RadioStationCategory    string    // Detail only
+	MovementArea            string    // Detail only
+	LicenseNumber           string    // Detail only
 }
 
-// RadioSpec is the structure consisting of radio wave type, frequency, and
-// antenna power.
-type RadioSpec struct {
-	RadioFormat string
-	Freq        float64
-	Power       float64
+type RegistrationList struct {
+	RegistrationInfo []RegistrationInfo
+	TotalCount       int
+	LastUpdateDate   time.Time
+}
+
+type RegistrationInfo struct {
+	No                     string
+	Name                   string
+	ValidTerms             string
+	ItCd                   string
+	RegistrationDate       string
+	Note                   string // Detail only
+	Address                string // Detail only
+	RadioEquipmentLocation string // Detail only
+	RadioEquipmentStandard string // Detail only
+	RegistrationNumber     string // Detail only
+	RadioSpec2             string // Detail only
 }
